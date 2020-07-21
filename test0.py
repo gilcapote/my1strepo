152f03655ff8a877823637ea526ffad3f60b7362
gil capote <gilcapote85@gmail.com>
2020-07-21 02:27:24 -0300

    commit2

--- test0.py
+++ test0.py
@@ -7,3 +7,11 @@ Created on Tue Jul 21 01:24:32 2020
 
 
 import pandas as pd 
+
+
+
+url = "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+df = pd.read_csv(url)
+
+
+print(df.head())
